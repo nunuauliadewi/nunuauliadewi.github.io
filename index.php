0cc8243fab51f82ed2868a5f847d2b538dd2e588
nunuauliadewi <104054452+nunuauliadewi@users.noreply.github.com>
2022-04-20 12:53:51 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<html >
+    <head>
+        <title>Selamat ulang tahun</title>
+    </head>
+    <body>
+        <div container>
+            <br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+            <center>
+                <h1><marquee behavior="" direction="">SELAMAT ULANG TAHUN A</marquee></h1>
+                <h1><marquee behavior="" direction="">AHMAD RIADOTUL FAWAID</marquee></h1>
+            </center>
+            <br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+            <h2>HALLOO AA!!!</h2>
+            <h4>
+            "Walaupun sering menjengkelkan, tapi kita selalu menyayangimu. Terima kasih atas waktumu yang  sudah dihabiskan untuk menasehati dan memberikan <br> bimbingan yang baik. Terima kasih atas segalanya yang telah engkau berikan kepada kita. Dari hati yang paling tulus, kita ucapkan selamat ulang tahun!" <br>  <br> 
+            "Terima kasih telah menjadi orang yang luar biasa. Dan terima kasih telah menjadi Aa terbaik untuk kita. Aa adalah hadiah sempurna yang Allah berikan <br> untuk membimbing kita melewati segala ujian dalam hidup."
+            </h4>
+        </div>
+    </body>
+</html>
